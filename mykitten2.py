59e81cfbcc82e68b8f9187a569f178d5620e78a2
zk <liuzq7@gmail.com>
2019-10-27 20:02:09 +0800

    update

--- mykitten2.py
+++ mykitten2.py
@@ -11,19 +11,19 @@ def handle_result(args, answer, target_window_id, boss):
 
       # send ctrl-c command
       # if neovim is 0.4.x , use write_to_child and the rest
-#       w.write_to_child("\3")
+      w.write_to_child("\3")
       # if neovim is 0.3.x ,use paste and the rest
-      w.paste("\3")
+#       w.paste("\3")
 
       # need to sleep a little,otherwise it doesn`t work well
       time.sleep(0.005)
       arg= args[1]
       if len(arg)>3 and arg[-2:] =="\\r":
-          w.paste(arg[:-2]+"\r")
+          w.write_to_child(arg[:-2]+"\r")
       elif len(arg)>4 and arg[:4] =="\\x20":
-          w.paste("\x20"+arg[4:])
+          w.write_to_child("\x20"+arg[4:])
       else:
-          w.paste(args[1])
+          w.write_to_child(args[1])
 
 
 handle_result.no_ui = True
